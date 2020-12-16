04f513d99bd637b2526dd9d65e28e71a46533def
skarthick2312 <karthicksamsung99@gmail.com>
2020-12-16 18:56:43 +0530

    second version

--- hungry/src/hungry/Hungry.java
+++ hungry/src/hungry/Hungry.java
@@ -15,7 +15,7 @@ public class Hungry {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Im hungry");
+        System.out.println("Are u hungry");
     }
     
 }
