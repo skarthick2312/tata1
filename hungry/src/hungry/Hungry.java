36457274005e57717ba9c37c7b48487fc36729a5
skarthick2312 <karthicksamsung99@gmail.com>
2020-12-17 14:10:12 +0530

    fourth

--- hungry/src/hungry/Hungry.java
+++ hungry/src/hungry/Hungry.java
@@ -15,7 +15,7 @@ public class Hungry {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("i am karthick");
+        System.out.println("i am working in tcs");
     }
     
 }
