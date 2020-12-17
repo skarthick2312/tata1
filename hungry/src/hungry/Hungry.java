107f92dbb85a572143af7808bdc663354ea08fd6
skarthick2312 <karthicksamsung99@gmail.com>
2020-12-17 14:13:08 +0530

    fifth

--- hungry/src/hungry/Hungry.java
+++ hungry/src/hungry/Hungry.java
@@ -16,6 +16,7 @@ public class Hungry {
      */
     public static void main(String[] args) {
         System.out.println("i am working in tcs");
+        System.out.println("i am working");
     }
     
 }
